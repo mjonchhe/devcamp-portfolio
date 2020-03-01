d51c51639c82b86b20ddbb50e2f8d8d9266a8cd5
Manju  Jonchhe <mjonchhe@gmail.com>
2020-02-29 22:34:39 -0500

    Integrated new gem into rails for copyright

--- Gemfile
+++ Gemfile
@@ -58,3 +58,5 @@ gem 'friendly_id', '~> 5.1.0'
 gem 'devise', '~> 4.7', '>= 4.7.1'
 
 gem 'bootstrap', '~> 4.0.0.alpha6'
+
+gem 'test_devcamp_view_tool', git: 'https://github.com/mjonchhe/test_devcamp_view_tool'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    TestDevcampViewTool::Renderer.copyright('MJ', 'All rights reserved')
+  end
 end
