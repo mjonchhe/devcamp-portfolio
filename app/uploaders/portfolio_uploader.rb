2cf3f506500c76d3f491d70ef5c014ec104c8d7b
Manju  Jonchhe <mjonchhe@gmail.com>
2020-03-14 19:56:43 -0400

    Finished carrierware installation

--- app/uploaders/portfolio_uploader.rb
+++ app/uploaders/portfolio_uploader.rb
@@ -1,6 +1,5 @@
 class PortfolioUploader < CarrierWave::Uploader::Base
-  storage :file
-  # storage :fog
+  storage :aws
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
