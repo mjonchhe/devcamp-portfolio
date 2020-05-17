0f6de044dae3a574353c38a6e9609cf8f86af925
Manju  Jonchhe <mjonchhe@gmail.com>
2020-05-16 21:45:43 -0400

    Implemented the blog widget for topics

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,7 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
+  before_action :set_sidebar_topics, except: [:update, :create, :destroy, :toggle_status]
+
   layout 'blog'
   access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
 
@@ -83,13 +85,12 @@ class BlogsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_blog
-      @blog = Blog.friendly.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def blog_params
-      params.require(:blog).permit(:title, :body, :topic_id)
-    end
+  def set_blog
+    @blog = Blog.friendly.find(params[:id])
+  end
+
+  def blog_params
+    params.require(:blog).permit(:title, :body, :topic_id)
+  end
 end
